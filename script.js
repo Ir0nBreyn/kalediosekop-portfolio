eec71157d9cf22bde30e6fcfa81771d8ce8edfb7
Ardhiya Febrian Rachman <ardhiyafebrianr@gmail.com>
2026-09-24 22:39:50 +0700

    Remove KAI Commuter.webp from C-Infra Outcome

--- script.js
+++ script.js
@@ -775,7 +775,6 @@
                                 </div>
                                 <div class="case-study-popup-section-images">
                                     <img src="/Asset/Home/Featured Work/FW - 2/5 - UI.webp" alt="C-Infra UI" style="width: 100%; border-radius: 4px; cursor: zoom-in;" onclick="event.stopPropagation(); openImagePopup(this.src)">
-                                    <img src="/Asset/Home/Featured Work/KAI Commuter.webp" alt="KAI Commuter outcome" onclick="openImagePopup(this.src)" style="cursor: zoom-in;">
                                 </div>
                             </div>
 
